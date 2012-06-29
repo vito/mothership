be56f3202c084a37ed46ecf5f3634d66c0945041
Alex Suraci <suraci.alex@gmail.com>
2012-06-28 23:57:00 -0400

    dynamically-scoped filtering

--- lib/mothership/callbacks.rb
+++ lib/mothership/callbacks.rb
@@ -1,4 +1,9 @@
 class Mothership
+  # temporary filters via #with_filters
+  #
+  # command => { tag => [callbacks] }
+  @@filters = Hash.new { |h, k| h[k] = Hash.new { |h, k| h[k] = [] } }
+
   class << self
     # register a callback that's evaluated before a command is run
     def before(name, &callback)
@@ -25,10 +30,37 @@ class Mothership
 
   # filter a value through any plugins
   def filter(tag, val)
+    if @@filters.key?(@command.name) &&
+         @@filters[@command.name].key?(tag)
+      @@filters[@command.name][tag].each do |f|
+        val = f.call val
+      end
+    end
+
     @command.filters[tag].each do |f|
       val = f.call val
     end
 
     val
+  end
+
+  # temporary dynamically-scoped filters
+  def with_filters(filters)
+    filters.each do |cmd, callbacks|
+      callbacks.each do |tag, callback|
+        @@filters[cmd][tag] << callback
+      end
+    end
+
+    yield
+  ensure
+    filters.each do |cmd, callbacks|
+      callbacks.each do |tag, callback|
+        @@filters[cmd][tag].delete callback
+      end
+
+      @@filters[cmd].delete(tag) if @@filters[cmd][tag].empty?
+      @@filters.delete(cmd) if @@filters[cmd].empty?
+    end
   end
 end
